0437cbd835af252e88480d9ef3519111e3cdb5a5
李远君 <1123431949@qq.com>
2019-10-19 15:12:32 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-image=${DOCKER_REGISTRY_HOST}/${NAME}:${TAG}
-sed -i "s#{{IMAGE}}#$image#g" ./kubernetes.yaml
-cat ./kubernetes.yaml
-
-kubectl get po
-kubectl delete -f ./kubernetes.yaml > /dev/null 2>&1
-kubectl apply -f ./kubernetes.yaml
