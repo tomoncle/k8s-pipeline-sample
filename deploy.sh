d9acaf1a4c2824023c29c23eb3d74501f595eb99
李远君 <1123431949@qq.com>
2019-10-19 14:17:43 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-kubectl delete -f ./kubernetes.yaml
-
 image=${DOCKER_REGISTRY_HOST}/${NAME}:${TAG}
 sed -i "s#{{IMAGE}}#$image#g" ./kubernetes.yaml
+cat ./kubernetes.yaml
+cat ~/.kube/config
+kubectl get po
+kubectl delete -f ./kubernetes.yaml
 kubectl apply -f ./kubernetes.yaml
